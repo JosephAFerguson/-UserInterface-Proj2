93b8c28f3c0ee1ef62c873a2fcf21b46faa2effd
elipappas <eli@bpi.net>
2025-10-15 18:53:25 -0400

    created boiler plate for main page

---
+++ UserInterfaceProj2/src/device-ui.svelte
@@ -0,0 +1,48 @@
+<script>
+  import { onMount } from 'svelte'
+</script>
+
+<main>
+  <div id="shelf-1">
+    <div id="books"></div>
+    <div id="lights">
+      <!-- put lights here -->
+    </div>
+  </div>
+  <div id="shelf-2">
+    <div id="books"></div>
+    <div id="lights">
+      <!-- put lights here -->
+    </div>
+  </div>
+  <div id="shelf-3">
+    <div id="books"></div>
+    <div id="lights">
+      <!-- put lights here -->
+    </div>
+  </div>
+  <div id="shelf-4">
+    <div id="books"></div>
+    <div id="lights">
+      <!-- put lights here -->
+    </div>
+  </div>
+</main>
+
+<style>
+  main {
+    background-color: #563232;
+    color: white;
+    width: 100%;
+    height: 100%;
+  }
+
+  #shelf-1, #shelf-2, #shelf-3, #shelf-4 {
+    margin-top: 5%;
+    margin-left: 5%;
+    margin-right: 5%;
+    width: 90%;
+    height: 18%;
+    background-color: black;
+  }
+</style>
---
+++ UserInterfaceProj2/src/testing-ui.svelte
@@ -0,0 +1,47 @@
+<script>
+</script>
+
+<main>
+    <div style="display:flex; align-items:center; width:90%;">
+        <h1>Testing UI</h1>
+        <button id='info-button'>i</button>
+    </div>
+    <button id='eye-button'>
+        <img src="./public/eye.png" alt="Eye Icon" width="20" height="20"/>
+    </button>
+    <br/>
+    <br/>
+    <div style="display:flex; gap:8px;">
+        <button type="button">+</button>
+        <p style="margin:0">Add Books</p>
+    </div>
+    <br/>
+    <div style="display:flex; gap:8px;">
+        <button type="button">-</button>
+        <p style="margin:0">Remove Books</p>
+    </div>
+    <br/>
+    <div style="display:flex; gap:8px;">
+        <p>Sort by:</p>
+        <select>
+            <option>None</option>
+            <option>A-Z</option>
+            <option>Z-A</option>
+            <option>Genre</option>
+        </select>
+    </div>
+    <br/>
+    <button>Run Simulation</button>
+    <br/>
+    <br/>
+    <button>Go to Data Display</button>
+</main>
+
+<style>
+    main {
+        margin-left: 5%;
+    }
+    #info-button {
+        margin-left: 5px;
+    }
+</style>
