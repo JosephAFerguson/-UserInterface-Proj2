79a8e1beb97c197856a5c91a36f3cd0f546c02f2
elipappas <eli@bpi.net>
2025-10-17 13:59:07 -0400

    updated styling for testing-ui

--- UserInterfaceProj2/src/testing-ui.svelte
+++ UserInterfaceProj2/src/testing-ui.svelte
@@ -2,22 +2,23 @@
 </script>
 
 <main>
-    <div style="display:flex; align-items:center; width:90%;">
-        <h1>Testing UI</h1>
-        <button id='info-button'>i</button>
-    </div>
+    <br/>
     <button id='eye-button'>
         <img src="./public/eye.png" alt="Eye Icon" width="20" height="20"/>
     </button>
-    <br/>
-    <br/>
+    <button id='info-button'>
+        <img src="./public/information-button.png" alt="Info Icon" width="20" height="20"/>
+    </button>
+    <div style="display:flex; align-items:center; width:90%;">
+        <h1>Testing UI</h1>
+    </div>
     <div style="display:flex; gap:8px;">
-        <button type="button">+</button>
+        <button type="button" id="add">+</button>
         <p style="margin:0">Add Books</p>
     </div>
     <br/>
     <div style="display:flex; gap:8px;">
-        <button type="button">-</button>
+        <button type="button" id="remove">-</button>
         <p style="margin:0">Remove Books</p>
     </div>
     <br/>
@@ -44,7 +45,22 @@
     main {
         margin-left: 5%;
     }
+
+    button, select {
+        cursor: pointer;
+    }
+
+    #eye-button, #info-button {
+        background: none;
+        border: none;
+    }
+
     #info-button {
-        margin-left: 5px;
+        float: right;
+        margin-right: 5%;
+    }
+
+    #add, #remove {
+        border-radius: 50%;
     }
 </style>
